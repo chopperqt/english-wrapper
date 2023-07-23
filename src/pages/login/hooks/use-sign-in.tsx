dbfbb81556ad25f63c9a666906eb4cf72e65cff4
chopperqt <ilua292010@gmail.com>
2023-07-23 21:42:16 +0600

    Fix bug when token not set

--- src/pages/login/hooks/use-sign-in.tsx
+++ src/pages/login/hooks/use-sign-in.tsx
@@ -5,6 +5,7 @@ import { login } from "../../../api/auth.api";
 import { useMessage } from "../../../utils/use-message";
 import { useDispatch } from "react-redux";
 import { setAuth } from "../../../stores/user.slice";
+import supabase from "../../../api";
 
 const SUCCESS_TEXT = "Logged success.";
 
@@ -45,6 +46,10 @@ export const useSignIn = ({ form }: UseSignInProps) => {
 
     setDisabled(true);
     handleShowSuccess(SUCCESS_TEXT);
+
+    const session = await supabase.auth.getSession();
+
+    localStorage.setItem("token", session.data.session?.refresh_token || "");
 
     setTimeout(() => {
       dispatch(setAuth(true));

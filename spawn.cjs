1b4313f708f5eb590829de5192c193a30b03eb8d
chopperqt <ilua292010@gmail.com>
2023-08-06 23:23:11 +0600

    Refactroing `spawn.cjs`

--- spawn.cjs
+++ spawn.cjs
@@ -1,34 +1,11 @@
-// const spawn = require("child_process").spawn;
-
-// const englishWrapper = spawn("npm run dev -- --host");
-//
-// const { exec } = require("child_process");
-//
-// exec("ls", (error, stdout, stderr) => {
-//   if (error) {
-//     console.log(`error: ${error.message}`);
-//
-//     return;
-//   }
-//
-//   if (stderr) {
-//     console.log(`stderr: ${stderr}`);
-//
-//     return;
-//   }
-//
-//   console.log(`Output: ${stdout}`);
-// });
-//
-
 const docker = "docker compose up -d";
 const englishWrapper = "(npm run dev -- --host&)";
 const englishLibrary = "cd .. && cd word-library && (npm run start&)";
 const englishRepeater = "cd .. && cd english-repeater && npm run start";
 
-var spawn = require("child_process").spawn;
+const spawn = require("child_process").spawn;
 
-var child = spawn(
+const child = spawn(
   `${docker} && ${englishWrapper} && ${englishLibrary} && ${englishRepeater}`,
   {
     shell: true,

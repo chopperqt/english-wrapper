1fef3e6344bdc599bfab83734431adb9d598b90c
Chopperqt <ilua292010@gmail.com>
2024-02-20 23:17:12 +0600

    Иньекция стора

--- src/stores/store.tsx
+++ src/stores/store.tsx
@@ -1,32 +1,34 @@
-import { combineReducers, configureStore, createStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./user.slice";
 import commonReducer from "./common.slice";
-import { statisticsApi } from "./statistics.slice";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
-const statiscReducer = {}
+const statiscReducer = {};
 
-export const createReducer = (asyncReducers = {}) => combineReducers({
-  ...asyncReducers,
-  ...statiscReducer,
-})
+export const createReducer = (asyncReducers = {}) =>
+  combineReducers({
+    ...asyncReducers,
+    ...statiscReducer,
+  });
 
 export const emptySplitApi = createApi({
   baseQuery: fetchBaseQuery(),
   endpoints: () => ({}),
-})
+});
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     common: commonReducer,
-    [statisticsApi.reducerPath]: statisticsApi.reducer,
+    // [statisticsApi.reducerPath]: statisticsApi.reducer,
     [emptySplitApi.reducerPath]: emptySplitApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(statisticsApi.middleware).concat(emptySplitApi.middleware),
+    getDefaultMiddleware().concat(emptySplitApi.middleware),
 });
+
+console.log("store: ", store);
 
 export type RootState = ReturnType<typeof store.getState>;
 

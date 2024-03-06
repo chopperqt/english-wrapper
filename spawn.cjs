0295d432cf185434b4b472482c42ec9d272616c4
Chopperqt <ilua292010@gmail.com>
2024-03-06 22:47:36 +0500

    Изменение spanw

--- spawn.cjs
+++ spawn.cjs
@@ -1,22 +1,26 @@
 // const docker = "docker compose up -d";
 // const englishLibrary = "cd .. && cd word-library && (npm run start&)";
 const englishWrapper = "(npm run dev&)";
-const englishRepeater = "cd .. && cd english-repeater && (npm run serve&) && (npm run build&)";
+const englishRepeater =
+  "cd .. && cd english-repeater && (npm run serve&) && (npm run build&)";
+const englishStatistics =
+  "cd .. && cd english-statistics && (npm run serve&) && (npm run build&)";
 
 const spawn = require("child_process").spawn;
 
-const child = spawn(`${englishWrapper} && ${englishRepeater}`,
+const child = spawn(
+  `${englishWrapper} && ${englishRepeater} && ${englishStatistics}`,
   {
     shell: true,
-  }
+  },
 );
 
-child.stderr.on("data", function(data) {
+child.stderr.on("data", function (data) {
   console.error("STDERR:", data.toString());
 });
-child.stdout.on("data", function(data) {
+child.stdout.on("data", function (data) {
   console.log("STDOUT:", data.toString());
 });
-child.on("exit", function(exitCode) {
+child.on("exit", function (exitCode) {
   console.log("Child exited with code: " + exitCode);
 });
